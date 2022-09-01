0c9bf01b66b8b88c4f74424aa099956a4c1e86e6
saritha6 <84068893+saritha6@users.noreply.github.com>
2022-09-01 10:35:00 +0530

    Create min.py

---
+++ min.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=[]
+for i in range(n):
+               t=int(input())
+               arr.append(t)
+print(arr)
+min=arr[0]
+for i in arr:
+               if(i<min):
+                              min=1
+print(min)   
