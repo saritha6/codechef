d9d4fe7f3d0d6f80bf9053e4f5b4aa3dd0d009ad
saritha6 <84068893+saritha6@users.noreply.github.com>
2022-09-01 11:29:16 +0530

    Delete joel.py

--- joel.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-arr=list(map(int,input().split()))
-res=0
-for i in arr:
-               if(i==3):
-                              res+=1
-               else:
-                              break
-print(n-res)
